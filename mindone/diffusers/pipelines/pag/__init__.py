b522b111cd06e2f31133d3a4cf9197670ced1cfc
Fzilan <Fzilan@users.noreply.github.com>
2024-10-29 19:18:12 +0800

    add pag sd xx pipelines

--- mindone/diffusers/pipelines/pag/__init__.py
+++ mindone/diffusers/pipelines/pag/__init__.py
@@ -5,9 +5,25 @@ from ...utils import _LazyModule
 _import_structure = {}
 
 _import_structure["pipeline_pag_controlnet_sd"] = ["StableDiffusionControlNetPAGPipeline"]
+_import_structure["pipeline_pag_controlnet_sd_xl"] = ["StableDiffusionXLControlNetPAGPipeline"]
+_import_structure["pipeline_pag_hunyuandit"] = ["HunyuanDiTPAGPipeline"]
+_import_structure["pipeline_pag_kolors"] = ["KolorsPAGPipeline"]
+_import_structure["pipeline_pag_pixart_sigma"] = ["PixArtSigmaPAGPipeline"]
+_import_structure["pipeline_pag_sd"] = ["StableDiffusionPAGPipeline"]
+_import_structure["pipeline_pag_sd_3"] = ["StableDiffusion3PAGPipeline"]
+_import_structure["pipeline_pag_sd_animatediff"] = ["AnimateDiffPAGPipeline"]
+_import_structure["pipeline_pag_sd_xl"] = ["StableDiffusionXLPAGPipeline"]
+_import_structure["pipeline_pag_sd_xl_img2img"] = ["StableDiffusionXLPAGImg2ImgPipeline"]
+_import_structure["pipeline_pag_sd_xl_inpaint"] = ["StableDiffusionXLPAGInpaintPipeline"]
 
 if TYPE_CHECKING:
+    from .pipeline_pag_controlnet_sd import StableDiffusionControlNetPAGPipeline
+    from .pipeline_pag_controlnet_sd_xl import StableDiffusionXLControlNetPAGPipeline
+    from .pipeline_pag_pixart_sigma import PixArtSigmaPAGPipeline
     from .pipeline_pag_sd import StableDiffusionPAGPipeline
+    from .pipeline_pag_sd_xl import StableDiffusionXLPAGPipeline
+    from .pipeline_pag_sd_xl_img2img import StableDiffusionXLPAGImg2ImgPipeline
+    from .pipeline_pag_sd_xl_inpaint import StableDiffusionXLPAGInpaintPipeline
 
 else:
     import sys
